e549e786e86f80e7c5a3c7b4b632d747576f7d30
apiwatapiwat <apiwat.k.potae@gmail.com>
2017-08-22 17:36:21 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,22 +1,2 @@
 <?php
-
-require('../routeros_api.class.php');
-
-$API = new RouterosAPI();
-
-$API->debug = false;
-
-if ($API->connect('192.168.30.1','admin', '')) {
-
-   $API->write('/interface/getall');
-
-   $READ = $API->read(false);
-   $ARRAY = $API->parseResponse($READ);
-
-   print_r($ARRAY);
-
-   $API->disconnect();
-
-}
-
-?>
+echo "I am a bot";
